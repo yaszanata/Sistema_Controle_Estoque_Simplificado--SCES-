e90a76d853ec7ac8da968d442bea79bc32ede919
Yasmin Zanata <yasmin.zanata@edu.senai.br>
2026-06-18 08:58:47 -0300

    Função de adicionar produto aprimorada, função de buscar produto criada

--- V1-SCES.py
+++ V1-SCES.py
@@ -9,10 +9,24 @@ estoque = []
 
 #funções:
 
-def adicionar_produto():
+def adicionar_produto(): ##adicionar verificamento para não aceitar produtos repetidos!!!!
     global estoque
     nome = input("Insira o nome produto: ")
+    nomeProcurada = -1
+
+    for i in range(len(estoque)): #varre linha por linha na matriz
+        if(estoque[i][1] == nome): #verifica se o nome já existe
+            nomeProcurada = i
+            
+            print(f"Esse produto já está no estoque! {nomeProcurada}")
+    
     id = input("Insira o ID do produto: ")
+    for i in range(len(estoque)): #varre linha por linha na matriz
+            if(estoque[i][1] == id): #verifica se o id já existe
+                idProcurada = i
+                
+                print(f"Esse ID já  foi cadastrado no estoque! {idProcurada}")
+
     qntEstoque = input("Insira a quantidade do produto presente no estoque: ")
     localizacao = input("Insira onde se localiza o produto: ")
 
@@ -23,13 +37,21 @@ def lista_estoque():
     print("-------------ESTOQUE--------------")
     print()
 
+def buscar_produto():
+    global id
+    produtoProcurado = input("Insira o ID do produto que deseja procurar: ")
+    colunaProcurada = -1
+
+    for i in range(len(estoque)): #varre linha por linha na matriz
+        if(estoque[i][0] == produtoProcurado): #verifica se a posição do id é igual ao id procurado
+            colunaProcurada = i
 
 
 ##menu
-    print("---------SISTEMA DE ESTOQUE--------")
+    print("\n---------SISTEMA DE ESTOQUE--------")
 while True: ##roda para sempre
    
-    print("\n1- Adicionar produto\n2- Listar todos os produtos  \n3- Buscar produto por ID \n4- Atualizar estoque  \n5- Sair do programa")
+    print("\n1- Adicionar produto\n2- Listar todos os produtos  \n3- Buscar produto por ID \n4- Atualizar estoque \n5- Remover produto do estoque  \n6- Sair do programa")
     opcao = input("R: ")
     print("-----------------------------------") 
     
@@ -39,3 +61,9 @@ while True: ##roda para sempre
 
     elif opcao == "2":
         lista_estoque()
+
+    elif opcao == "3":
+        buscar_produto()
+
+    elif opcao == "4":
+        print("")
