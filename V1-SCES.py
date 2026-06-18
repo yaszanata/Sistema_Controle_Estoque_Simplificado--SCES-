e3cda80238479e568faf165335fe7a1cc4e6a4a9
Yasmin Zanata <yasmin.zanata@edu.senai.br>
2026-06-18 09:42:00 -0300

    Função de travar menu adicionada, função de procurar ID aprimorada para identificar se o ID não estiver cadastrado

--- V1-SCES.py
+++ V1-SCES.py
@@ -4,56 +4,71 @@
 #coluna 2 - Quantidade em Estoque 
 #coluna 3 - Localização 
 
-estoque = []
-    
+estoque = [
+    [0, "arroz", 33, 12]
+]
+id = 0  
 
 #funções:
-
-def adicionar_produto(): ##adicionar verificamento para não aceitar produtos repetidos!!!!
+def travarMenu():
+    input("\nPressione <ENTER> para continuar...")
+    
+def adicionar_produto():
     global estoque
+    global id 
+
     nome = input("Insira o nome produto: ")
-    nomeProcurada = -1
+    nomeProcurado = -1
 
     for i in range(len(estoque)): #varre linha por linha na matriz
         if(estoque[i][1] == nome): #verifica se o nome já existe
-            nomeProcurada = i
-            
-            print(f"Esse produto já está no estoque! {nomeProcurada}")
-    
-    id = input("Insira o ID do produto: ")
-    for i in range(len(estoque)): #varre linha por linha na matriz
-            if(estoque[i][1] == id): #verifica se o id já existe
-                idProcurada = i
-                
-                print(f"Esse ID já  foi cadastrado no estoque! {idProcurada}")
+            nomeProcurado = i
+            print(f"Esse produto já está no estoque!")
 
-    qntEstoque = input("Insira a quantidade do produto presente no estoque: ")
-    localizacao = input("Insira onde se localiza o produto: ")
+        if nomeProcurado == -1:
 
-    estoque.append([id, nome, qntEstoque, localizacao])
-    print("Produto registrado com sucesso!")
+            id = id + 1
+            qntEstoque = int(input("Insira a quantidade do produto presente no estoque: "))
+            localizacao = input("Insira onde se localiza o produto: ")
+
+            estoque.append([id, nome, qntEstoque, localizacao])
+            print("Produto registrado com sucesso!")
+
+    travarMenu()
+
 
 def lista_estoque():
     print("-------------ESTOQUE--------------")
-    print()
+    for linha in estoque:
+        print(linha)
+
+    travarMenu()
 
 def buscar_produto():
     global id
     produtoProcurado = input("Insira o ID do produto que deseja procurar: ")
     colunaProcurada = -1
 
-    for i in range(len(estoque)): #varre linha por linha na matriz
+    for i in range(len(estoque)): 
         if(estoque[i][0] == produtoProcurado): #verifica se a posição do id é igual ao id procurado
             colunaProcurada = i
+            print(f"\nO ID {produtoProcurado} procurado está na linha {colunaProcurada}")
+
+        else:
+            ("O ID procurado não está registrado!")
+
+    travarMenu()
+
+
 
 
 ##menu
     print("\n---------SISTEMA DE ESTOQUE--------")
 while True: ##roda para sempre
    
-    print("\n1- Adicionar produto\n2- Listar todos os produtos  \n3- Buscar produto por ID \n4- Atualizar estoque \n5- Remover produto do estoque  \n6- Sair do programa")
+    print("\n1- Adicionar produto\n2- Listar todos os produtos  \n3- Buscar produto por ID \n4- Atualizar quantidade do produto no estoque \n5- Remover produto do estoque  \n6- Sair do programa")
     opcao = input("R: ")
-    print("-----------------------------------") 
+    print("-----------------------------------\n") 
     
 
     if opcao == "1":
